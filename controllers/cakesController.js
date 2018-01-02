1291e36ed36147fcc801f7b78c563ca61228fa6a
William Rainaud <wrainaud@gmail.com>
2018-01-02 14:58:02 -0500

    Refactoring Code

--- controllers/cakesController.js
+++ controllers/cakesController.js
@@ -1,37 +1,68 @@
 var express = require("express");
 
 var router = express.Router();
-var cake = require("../models/cake.js");
+
+var db = require("../models");
 
 router.get("/", function(req, res) {
     res.redirect("/cakes");
 });
 
 router.get("/cakes", function(req, res) {
-    cake.all(function(cakeData) {
-        res.render("index", { cake_data: cakeData });
-    });
+    db.Cake.findAll({
+            include: [db.Customer],
+            order: [
+                ["cake_name", "ASC"]
+            ]
+        })
+        .then(function(dbCake) {
+            var hbsObject = {
+                burger: dbCake
+            };
+            return res.render("index", hbsObject);
+        });
 });
 
 router.post("/cakes/create", function(req, res) {
-    cake.create(req.body.cake_name, function(result) {
-        console.log(result);
-        res.redirect("/");
-    });
+    db.Cake.create({
+            cake_name: req.body.cake_name
+        })
+        .then(function(dbCake) {
+            console.log(dbCake);
+            res.redirect("/");
+        });
 });
 
 router.put("/cakes/update", function(req, res) {
-    cake.update(req.body.cake_id, function(result) {
-        console.log(result);
-        res.redirect("/");
-    });
-});
-
-router.delete("/cakes/update", function(req, res) {
-    cake.delete(req.body.cake_id, function(result) {
-        console.log(result);
-        res.redirect("/");
-    });
+    if (req.body.customer) {
+        db.Customer.create({
+                customer: req.body.customer,
+                CakeId: req.body.cake_id
+            })
+            .then(function(dbCustomer) {
+                return db.Cake.update({
+                    devoured: true
+                }, {
+                    where: {
+                        id: req.body.cake_id
+                    }
+                });
+            })
+            .then(function(dbCake) {
+                res.redirect("/");
+            });
+    } else {
+        db.Cake.update({
+                devoured: true
+            }, {
+                where: {
+                    id: req.body.cake_id
+                }
+            })
+            .then(function(dbCake) {
+                res.redirect("/");
+            });
+    }
 });
 
 module.exports = router;
