df6ac695f65da57667b235532e029cb826f913d3
William Rainaud <wrainaud@gmail.com>
2018-01-02 16:28:51 -0500

    Broken -  Sequelize Eager Loading Error

--- models/customer.js
+++ models/customer.js
@@ -2,7 +2,14 @@ module.exports = function(sequelize, DataTypes) {
     var Customer = sequelize.define("Customer", {
         customer: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true
+        }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                Customer.hasMany(models.Cake, { as: 'cakes' })
+            }
         }
     });
     return Customer;
